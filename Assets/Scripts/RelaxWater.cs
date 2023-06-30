4c35b38f48646044bc1a8a5e5c2c02fada926844
Odd-Doc <gsphillipsjr@gmail.com>
2023-06-30 08:41:10 -0500

    Update RelaxWater.cs
    Updated RelaxWater.cs to accommodate AssemblyContrller_Touch.cs

--- Assets/Scripts/RelaxWater.cs
+++ Assets/Scripts/RelaxWater.cs
@@ -2,36 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using com.zibra.liquid.Manipulators;
+
 public class RelaxWater : MonoBehaviour
 {
     [SerializeField]
     AssemblyController assemblyController;
+
+    [SerializeField]
+    AssemblyController_Touch assemblyController_Touch;
+
     [SerializeField]
     ZibraLiquidForceField checkValve1ForceField;
+
     [SerializeField]
     ZibraLiquidForceField checkValve2ForceField;
 
-    private void checkRelax(){
-        if(assemblyController.IsSupplyOn == false){
+    private void checkRelax()
+    {
+        if (assemblyController.IsSupplyOn == false || assemblyController_Touch.IsSupplyOn == false)
+        {
             checkValve1ForceField.enabled = false;
-            checkValve2ForceField.enabled = false;        
-            }
-            else{
-                checkValve1ForceField.enabled = true;
-                checkValve2ForceField.enabled = true;
-            }
+            checkValve2ForceField.enabled = false;
+        }
+        else
+        {
+            checkValve1ForceField.enabled = true;
+            checkValve2ForceField.enabled = true;
+        }
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
     // Update is called once per frame
     void Update()
     {
         checkRelax();
-        
     }
 }
