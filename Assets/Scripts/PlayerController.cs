d91a1b2d000be9e03865188fb451a31f4d6e1662
Odd-Doc <gsphillipsjr@gmail.com>
2023-07-08 14:38:20 -0500

    created CameraController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    PlayerInputAction playerInput;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        playerInput = new PlayerInputAction();
+        Checkvalve1InitPos = CheckValve1.transform.localPosition;
+        Checkvalve2InitPos = CheckValve2.transform.localPosition;
+        testCockClosedScale = TestCockValve1.transform.localScale;
+        testCockClosedYScale = testCockClosedScale.y;
+
+        //touch input
+
+        //Touch0
+        playerInput.Touchscreen.Touch0Contact.started += Touch0Contact_started;
+        playerInput.Touchscreen.Touch0Contact.canceled += Touch0Contact_canceled;
+        playerInput.Touchscreen.Touch0Delta.started += Touch0Delta_started;
+        playerInput.Touchscreen.Touch0Delta.canceled += Touch0Delta_canceled;
+        //Touch1
+        playerInput.Touchscreen.Touch1Contact.started += Zoom_started;
+        playerInput.Touchscreen.Touch1Contact.canceled += Zoom_canceled;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        playerInput.Enable();
     }
+
+    void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
 }
