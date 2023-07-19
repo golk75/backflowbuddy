893959e4cfcd2c0fbb170e3a4b1964ba483d7d89
Odd-Doc <gsphillipsjr@gmail.com>
2023-07-19 14:15:06 -0500

    added baked stated for filled/ primed device

--- Assets/Scripts/FillButton.cs
+++ Assets/Scripts/FillButton.cs
@@ -1,21 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using com.zibra.liquid.Manipulators;
-using com.zibra.liquid.Analytics;
-using com.zibra.liquid.Solver;
 
 public class FillButton : MonoBehaviour
 {
-    ZibraLiquid liquid;
-
-    public void FillDevice()
-    {
-        liquid.InitialState = ZibraLiquid.InitialStateType.BakedLiquidState;
-    }
+    public GameObject Check1;
+    public GameObject Check2;
 
     // Start is called before the first frame update
     void Start() { }
+
+    public void FillDevice()
+    {
+        Check1.transform.localPosition = new Vector3(-0.10f, 0, -0.08f);
+        Check2.transform.localPosition = new Vector3(-0.20f, -2.25f, -0.17f);
+    }
 
     // Update is called once per frame
     void Update() { }
