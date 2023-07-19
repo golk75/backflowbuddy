c9046045653d7495f82f49661d6c2054e3098f69
Odd-Doc <gsphillipsjr@gmail.com>
2023-07-19 14:32:18 -0500

    Added functional fill button for testing

--- Assets/Scripts/FillButton.cs
+++ Assets/Scripts/FillButton.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using com.zibra.liquid.Solver;
 
 public class FillButton : MonoBehaviour
 {
+    public ZibraLiquid liquid;
     public GameObject Check1;
     public GameObject Check2;
 
@@ -12,6 +14,12 @@ public class FillButton : MonoBehaviour
 
     public void FillDevice()
     {
+        liquid.ReleaseSimulation();
+        liquid.InitialState = ZibraLiquid.InitialStateType.BakedLiquidState;
+        liquid.enabled = false;
+        liquid.enabled = true;
+        liquid.InitializeSimulation();
+
         Check1.transform.localPosition = new Vector3(-0.10f, 0, -0.08f);
         Check2.transform.localPosition = new Vector3(-0.20f, -2.25f, -0.17f);
     }
