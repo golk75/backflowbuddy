d6d158a66fe424f6220b7e2f69da05531403e76f
Odd-Doc <gsphillipsjr@gmail.com>
2023-07-24 09:46:05 -0500

    decent changes to config joints
    removed auto config connected anchor

--- Assets/Scripts/WaterController.cs
+++ Assets/Scripts/WaterController.cs
@@ -65,9 +65,10 @@ public class WaterController : MonoBehaviour
     [SerializeField]
     ZibraLiquidVoid Void_Check2;
 
-    Vector3 Zone2VoidMaxSize = new Vector3(0.04f, 0.02f, 0.02f);
+    //Vector3 Zone2VoidMaxSize = new Vector3(0.045f, 0.035f, 0.02f);
+    Vector3 Zone2VoidMaxSize = new Vector3(0.0401f, 0.0354f, 0.02f);
 
-    Vector3 Zone3VoidMaxSize = new Vector3(0.04f, 0.02f, 0.02f);
+    Vector3 Zone3VoidMaxSize = new Vector3(0.0401f, 0.0354f, 0.02f);
     private ZibraLiquidVoid _zoneVoid;
     public ZibraLiquidVoid ZoneVoid
     {
