e98de0610757439e1d6a0b4d946bd6b6d0c14a9e
Odd-Doc <gsphillipsjr@gmail.com>
2023-08-01 08:32:20 -0500

    add low bleed and high bleed knobs

--- Assets/Scripts/TestKitController.cs
+++ Assets/Scripts/TestKitController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class TestKitController : MonoBehaviour
 {
+    public WaterController waterController;
+    public GameObject lowBleed;
+    public GameObject lowControl;
+
+    public GameObject highBleed;
+    public GameObject highControl;
+
+    public GameObject bypassControl;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 }
