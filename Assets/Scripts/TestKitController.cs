87aee85289c621c8b425818fc830ab4a7737641f
Odd-Doc <gsphillipsjr@gmail.com>
2023-08-01 10:25:11 -0500

    added tags to knobs

--- Assets/Scripts/TestKitController.cs
+++ Assets/Scripts/TestKitController.cs
@@ -16,27 +16,71 @@ public class TestKitController : MonoBehaviour
 
     public GameObject needle;
 
-    Vector3 initNeedleRot;
-    Vector3 endNeedleRot = new Vector3(0, 0, 255);
-    Vector3 currentNeedleRot;
+    private const float MinNeedle_rotation = 11;
+    private const float MaxNeedle_rotation = -101;
+    private const float MinKnob_rotation = 0;
+
+    //limit knobs to 4 complete rotations (x1 rotation = 360;)->
+    private const float MaxKnob_rotation = 1440;
+
+    private float currentKnobRotation;
+    private float maxKnobRotation;
+
+    private float currentPSID;
+    private float maxPSID;
 
     // Start is called before the first frame update
     void Start()
     {
-        initNeedleRot = needle.transform.eulerAngles;
+        currentPSID = 0;
+        maxPSID = 15;
+        currentKnobRotation = 0;
+        maxKnobRotation = 1440;
+    }
+
+    private float GetPsidNeedleRotation()
+    {
+        float PsidDiff = MinNeedle_rotation - MaxNeedle_rotation;
+
+        float normalizedPsid = currentPSID / maxPSID;
+
+        return MinNeedle_rotation - normalizedPsid * PsidDiff;
+    }
+
+    private float GetKnobRoation()
+    {
+        // max - min to rotate left while increasing
+        float rotationDiff = MaxKnob_rotation - MinKnob_rotation;
+
+        float normalizedRotation = currentKnobRotation / maxKnobRotation;
+
+        return MinKnob_rotation + normalizedRotation * rotationDiff;
+    }
+
+    private void DetectKnob()
+    {
+        switch (playerController.OperableObject.tag)
+        {
+            default:
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerController.OperableObjectRotation != null)
+        /*
+        currentPSID += 1 * Time.deltaTime;
+        if (currentPSID > maxPSID)
         {
-            currentNeedleRot = Vector3.Lerp(
-                initNeedleRot,
-                endNeedleRot,
-                Mathf.Clamp(playerController.OperableObjectRotation.z, 0, 1)
-            );
-            needle.transform.eulerAngles = currentNeedleRot;
+            currentPSID = maxPSID;
+        }
+        needle.transform.eulerAngles = new Vector3(0, 0, GetPsidNeedleRotation());
+        */
+        currentKnobRotation += 1 * Time.deltaTime;
+        if (currentKnobRotation > maxKnobRotation)
+        {
+            currentKnobRotation = maxKnobRotation;
         }
     }
 }
