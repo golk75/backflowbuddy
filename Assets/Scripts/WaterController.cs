e33610be791cdf6a1c87bbf40b838b3951cd2b04
Odd-Doc <gsphillipsjr@gmail.com>
2023-08-07 10:29:50 -0500

    fixed check house force field while testing check valve closing points
    set to zero if any check is closed

--- Assets/Scripts/WaterController.cs
+++ Assets/Scripts/WaterController.cs
@@ -408,12 +408,12 @@ public class WaterController : MonoBehaviour
                 ref check1VoidTC1Ref,
                 5f
             );
-            if (
-                TestCockHoseDetect2.isConnected == true
-                && testCockController.isTestCock3Open == true
-            )
+
+            //Regulate check housing force fields while shut off is closed (or testing is in progress)
+            if (checkValveStatus.isCheck1Closed || checkValveStatus.isCheck2Closed)
             {
                 check1housingForceField.Strength = 0;
+                check2housingForceField.Strength = 0;
             }
             else
             {
@@ -423,19 +423,6 @@ public class WaterController : MonoBehaviour
                     ref check1FFref.x,
                     0.2f
                 );
-            }
-            if (
-                TestCockHoseDetect3.isConnected == true
-                && testCockController.isTestCock4Open == true
-            )
-            {
-                check2housingForceField.Strength = 0;
-            }
-            else if (
-                TestCockHoseDetect3.isConnected == true
-                && testCockController.isTestCock4Open == true
-            )
-            {
                 check2housingForceField.Strength = Mathf.SmoothDamp(
                     check2housingForceField.Strength,
                     1f,
@@ -443,6 +430,43 @@ public class WaterController : MonoBehaviour
                     1f
                 );
             }
+            /*
+             if (
+                 TestCockHoseDetect2.isConnected == true
+                 && testCockController.isTestCock3Open == true
+             )
+             {
+                 check1housingForceField.Strength = 0;
+             }
+             else
+             {
+                 check1housingForceField.Strength = Mathf.SmoothDamp(
+                     check1housingForceField.Strength,
+                     1.2f,
+                     ref check1FFref.x,
+                     0.2f
+                 );
+             }
+             if (
+                 TestCockHoseDetect3.isConnected == true
+                 && testCockController.isTestCock4Open == true
+             )
+             {
+                 check2housingForceField.Strength = 0;
+             }
+             else if (
+                 TestCockHoseDetect3.isConnected == true
+                 && testCockController.isTestCock4Open == true
+             )
+             {
+                 check2housingForceField.Strength = Mathf.SmoothDamp(
+                     check2housingForceField.Strength,
+                     1f,
+                     ref check2FFref.x,
+                     1f
+                 );
+             }
+             */
         }
         else if (shutOffValveController.IsSupplyOn == true)
         {
