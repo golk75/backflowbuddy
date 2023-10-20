c313dc437c93b8738c3ebdf6f0f518c8cc547dd2
Odd-Doc <gsphillipsjr@gmail.com>
2023-10-20 15:33:27 -0500

    Back to Build Ready!

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -17,16 +17,16 @@ public class CursorController : MonoBehaviour
         root = GetComponent<UIDocument>();
         buttonWrapper = root.rootVisualElement.Q<VisualElement>("ButtonWrapper");
         sceneContainer = root.rootVisualElement.Q<VisualElement>("SceneContainer");
-        // cursor_grab = new()
-        // {
-        //     texture = Resources.Load<Texture2D>("UI/Textures/icons8-hand-34"),
-        //     hotspot = new Vector2(15, 12)
-        // };
-        // cursor_default = new()
-        // {
-        //     texture = Resources.Load<Texture2D>("UI/Textures/icons8-hand-32"),
-        //     hotspot = new Vector2(15, 12)
-        // };
+        cursor_grab = new()
+        {
+            texture = Resources.Load<Texture2D>("UI/Textures/icons8-hand-34"),
+            hotspot = new Vector2(15, 12)
+        };
+        cursor_default = new()
+        {
+            texture = Resources.Load<Texture2D>("UI/Textures/icons8-hand-32"),
+            hotspot = new Vector2(15, 12)
+        };
 
         sceneContainer.RegisterCallback<MouseDownEvent>(MouseDown);
         sceneContainer.RegisterCallback<MouseUpEvent>(MouseUp);
@@ -38,15 +38,15 @@ public class CursorController : MonoBehaviour
 
     private void MouseUp(MouseUpEvent evt)
     {
-        // sceneContainer.style.cursor = new StyleCursor(cursor_default);
-        // buttonWrapper.style.cursor = new StyleCursor(cursor_default);
+        sceneContainer.style.cursor = new StyleCursor(cursor_default);
+        buttonWrapper.style.cursor = new StyleCursor(cursor_default);
     }
 
     private void MouseDown(MouseDownEvent evt)
     {
 
-        // sceneContainer.style.cursor = new StyleCursor(cursor_grab);
-        // buttonWrapper.style.cursor = new StyleCursor(cursor_grab);
+        sceneContainer.style.cursor = new StyleCursor(cursor_grab);
+        buttonWrapper.style.cursor = new StyleCursor(cursor_grab);
 
 
     }
