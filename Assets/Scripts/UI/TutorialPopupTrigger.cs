d7bbca0d91839f265f0444ef7b3bfc5813f203f0
Odd-Doc <gsphillipsjr@gmail.com>
2024-01-22 13:33:06 -0600

    Tutorial set up cont

---
+++ Assets/Scripts/UI/TutorialPopupTrigger.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TutorialPopupTrigger : MonoBehaviour
+{
+    private const string TutorialContainerString = "TutorialPopup";
+    private VisualElement m_Tutorial_container;
+    public GameObject m_GameUi;
+    private UIDocument root;
+
+    public TutorialPopUpScriptableObject fillButtonTutorial;
+    public TutorialPopUpScriptableObject menuButtonTutorial;
+    public TutorialPopUpScriptableObject resetButtonTutorial;
+    public TutorialPopUpScriptableObject clickEnableTutorial;
+    public TutorialPopUpScriptableObject pauseButtonTutorial;
+    public TutorialPopUpScriptableObject playButtonTutorial;
+
+
+    private void Awake()
+    {
+        root = m_GameUi.GetComponent<UIDocument>();
+        m_Tutorial_container = root.rootVisualElement.Q<VisualElement>(TutorialContainerString);
+        // if (PlayerPrefs.GetInt("Tutorial Skip") == 0)
+        // {
+        //     m_Tutorial_container.style.display = DisplayStyle.Flex;
+        // }
+        // else
+        // {
+        //     m_Tutorial_container.style.display = DisplayStyle.None;
+        // }
+        m_Tutorial_container.style.display = DisplayStyle.Flex;
+
+    }
+
+    // public GameObject[] popUps;
+
+    // public VisualElement[] popUps;
+
+    // int popUpIndex;
+
+    //constants
+
+
+
+    void Update()
+    {
+
+    }
+}
