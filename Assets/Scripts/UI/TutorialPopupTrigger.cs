7dc55814fdd51d65b1d66a4a25112b050630d3bf
Odd-Doc <gsphillipsjr@gmail.com>
2024-01-23 08:42:04 -0600

    next and skip buttons prep (tutorial popup)

--- Assets/Scripts/UI/TutorialPopupTrigger.cs
+++ Assets/Scripts/UI/TutorialPopupTrigger.cs
@@ -7,20 +7,37 @@ using UnityEngine.UIElements;
 
 public class TutorialPopupTrigger : MonoBehaviour
 {
+    //visual element constants
     private const string TutorialContainerString = "TutorialPopup";
+    private const string TutorialNextButtonString = "Tutotial_next_button";
+
+    //visual elements
     private VisualElement m_Tutorial_container;
+
+    //gameobjects
     public GameObject m_GameUi;
+
+    //root
     private UIDocument root;
 
     public TutorialPopUpScriptableObject[] PopupScriptableObjects;
 
     public int popupIndex = 0;
+    public int skipTutPlayerPrefTestInt = 0;
 
     private void Awake()
     {
         root = m_GameUi.GetComponent<UIDocument>();
         m_Tutorial_container = root.rootVisualElement.Q<VisualElement>(TutorialContainerString);
-        // if (PlayerPrefs.GetInt("Tutorial Skip") == 0)
+        if (PlayerPrefs.GetInt("Tutorial Skip") == 0)
+        {
+            m_Tutorial_container.style.display = DisplayStyle.Flex;
+        }
+        else
+        {
+            m_Tutorial_container.style.display = DisplayStyle.None;
+        }
+        // if (skipTutPlayerPrefTestInt == 0)
         // {
         //     m_Tutorial_container.style.display = DisplayStyle.Flex;
         // }
@@ -28,7 +45,7 @@ public class TutorialPopupTrigger : MonoBehaviour
         // {
         //     m_Tutorial_container.style.display = DisplayStyle.None;
         // }
-        m_Tutorial_container.style.display = DisplayStyle.Flex;
+
 
     }
 
