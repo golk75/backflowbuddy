3f697c5b23d38087a1f2d3c7b16b7dab1166d815
Odd-Doc <gsphillipsjr@gmail.com>
2024-04-16 13:18:45 -0500

    Update QuizManager.cs

--- Assets/Resources/UI/CustomControls/Quiz/QuizManager.cs
+++ Assets/Resources/UI/CustomControls/Quiz/QuizManager.cs
@@ -148,7 +148,7 @@ public class QuizManager : VisualElement
     {
 
         // Actions.onQuizSelection?.Invoke(25);
-        Actions.onQuizSelection?.Invoke(2);
+        Actions.onQuizSelection?.Invoke(25);
         m_QuestionAndAnswerScreen.style.display = DisplayStyle.Flex;
         m_QuizSelectionScreen.style.display = DisplayStyle.None;
 
